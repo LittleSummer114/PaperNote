4c09f7cc901bbcfb0b5657800d87196a917bd33a
LittleSummer114 <582326366@qq.com>
2018-03-01 09:44:44 +0000

    Update run_all.py

--- run_all.py
+++ run_all.py
@@ -2,6 +2,13 @@
 import os
 '''
 1. 确定 learning rate
+2. 分别运行word char
+3. 尝试看看length_feature有没有用
+4. 尝试修改dimension
+5. 修改filter_size
+6. 修改max_features
+
+1. 修改模型结构,变得更宽更深
 '''
 models=[
     'CNN'
@@ -12,16 +19,30 @@ modes=[
 datasets=[
     'Travel'
 ]
+levels=[
+   # 'word',
+    'char'
+]
+length_features=[
+    0,
+    #1
+]
 wvs=[
     'trained'
 ]
 epochs=[
-    50
+    100
 ]
 batch_sizes=[
     500
              ]
 learning_rates=[
+    0.01,
+    0.03,
+    0.001,
+    0.003,
+    0.1,
+    0.3,
     1
 ]
 metrics=['RMSE']
@@ -29,10 +50,10 @@ dimensions=[
     256
 ]
 max_sent_word_lengths=[
-    200
+    40
 ]
 max_sent_char_lengths=[
-    200
+    60
 ]
 filter_sizes=[
     ['3','4','5']
@@ -41,7 +62,7 @@ num_of_filters=[
     ['100','100','100']
 ]
 gpu={'Travel':3}
-optimizers=['Adadelta','SGD']
+optimizers=['Adadelta']
 
 if __name__=='__main__':
     for dataset in datasets:
@@ -57,7 +78,9 @@ if __name__=='__main__':
                                             for num_of_filter in num_of_filters:
                                                 for optimizer in optimizers:
                                                     for max_sent_char_length in max_sent_char_lengths:
-                                                        os.system('python3 run.py --model {} '
+                                                        for level in levels:
+                                                            for length_feature in length_features:
+                                                                os.system('python3 run.py --model {} '
                                                                   '--mode {} '
                                                                   '--wv {} '
                                                                   '--batch_size {} '
@@ -68,7 +91,11 @@ if __name__=='__main__':
                                                                   '--max_sent_char_length {} '
                                                                   '--filter_size {} '
                                                                   '--number_of_filters {} '
-                                                                  '--optimizer {}'
+                                                                  '--optimizer {} '
+                                                                  '--level {} '
+                                                                  '--length_feature {} '
+                                                                  '--gpu {} '
+                                                                  '--dataset {}'
                                                                   ''.format(
                                                             model,
                                                             mode,
@@ -81,6 +108,9 @@ if __name__=='__main__':
                                                             max_sent_char_length,
                                                             ' '.join(filter_size),
                                                             ' '.join(num_of_filter),
-                                                            optimizer
+                                                            optimizer,
+                                                            level,
+                                                            length_feature,
+                                                            gpu[dataset],
+                                                            dataset
                                                         ))
-
